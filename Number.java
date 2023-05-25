28acf2d0e54b65fd409b3912e7ccd44d9fe7009a
Ahmet Tarık Fırat <113821940+trkfrt@users.noreply.github.com>
2023-05-25 03:50:09 +0300

    Create Number.java

---
+++ Number.java
@@ -0,0 +1,14 @@
+public class Number {
+    char name;
+    int numberOfNeighbor;
+    char neighbor[];
+    
+    public Number(char name,int numberOfNeighbor,char[] neighbor)
+    {
+        this.name = name;
+        this.numberOfNeighbor = numberOfNeighbor;
+        this.neighbor = neighbor;
+    }
+    
+    
+}
